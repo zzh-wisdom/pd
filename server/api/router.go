e88372a2423324db2070cba2139a5fa531fe6145
Ryan Leung <rleungx@gmail.com>
2019-12-05 17:20:08 +0800

    Improve the performance of the HTTP API for getting all regions (#1970) (#1986)
    * /api/v1/regions optimization (#1970)
    
    Signed-off-by: Ryan Leung <rleungx@gmail.com>

--- server/api/router.go
+++ server/api/router.go
@@ -22,12 +22,20 @@ import (
 	"github.com/unrolled/render"
 )
 
-const pingAPI = "/ping"
+func createStreamingRender() *render.Render {
+	return render.New(render.Options{
+		StreamingJSON: true,
+	})
+}
 
-func createRouter(prefix string, svr *server.Server) *mux.Router {
-	rd := render.New(render.Options{
+func createIndentRender() *render.Render {
+	return render.New(render.Options{
 		IndentJSON: true,
 	})
+}
+
+func createRouter(prefix string, svr *server.Server) *mux.Router {
+	rd := createIndentRender()
 
 	router := mux.NewRouter().PathPrefix(prefix).Subrouter()
 	handler := svr.GetHandler()
@@ -88,8 +96,11 @@ func createRouter(prefix string, svr *server.Server) *mux.Router {
 	router.HandleFunc("/api/v1/region/id/{id}", regionHandler.GetRegionByID).Methods("GET")
 	router.HandleFunc("/api/v1/region/key/{key}", regionHandler.GetRegionByKey).Methods("GET")
 
+	srd := createStreamingRender()
+	regionsAllHandler := newRegionsHandler(svr, srd)
+	router.HandleFunc("/api/v1/regions", regionsAllHandler.GetAll).Methods("GET")
+
 	regionsHandler := newRegionsHandler(svr, rd)
-	router.HandleFunc("/api/v1/regions", regionsHandler.GetAll).Methods("GET")
 	router.HandleFunc("/api/v1/regions/key", regionsHandler.ScanRegionsByKey).Methods("GET")
 	router.HandleFunc("/api/v1/regions/store/{id}", regionsHandler.GetStoreRegions).Methods("GET")
 	router.HandleFunc("/api/v1/regions/writeflow", regionsHandler.GetTopWriteFlow).Methods("GET")
@@ -136,7 +147,7 @@ func createRouter(prefix string, svr *server.Server) *mux.Router {
 	logHanler := newlogHandler(svr, rd)
 	router.HandleFunc("/api/v1/admin/log", logHanler.Handle).Methods("POST")
 
-	router.HandleFunc(pingAPI, func(w http.ResponseWriter, r *http.Request) {}).Methods("GET")
+	router.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {}).Methods("GET")
 	router.Handle("/health", newHealthHandler(svr, rd)).Methods("GET")
 	router.Handle("/diagnose", newDiagnoseHandler(svr, rd)).Methods("GET")
 	return router
